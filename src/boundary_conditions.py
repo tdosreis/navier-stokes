d5d30406a95b9c7dbece3f12220e4173fd47f3aa
tdosreis <tiagor.reis@gmail.com>
2022-01-25 14:28:44 -0300

    Fix style

--- src/boundary_conditions.py
+++ src/boundary_conditions.py
@@ -1,21 +1,13 @@
 def pressure_bc(Nxc, Nyc, c):
 
     c[2:Nxc, 2:Nyc] = 1.0/4.0
-
     c[1, 1] = 1.0/2.0
-
     c[Nxc, 1] = 1.0/2.0
-
     c[1, Nyc] = 1.0/2.0
-
     c[Nxc, Nyc] = 1.0/2.0
-
     c[2:Nxc, 1] = 1.0/3.0
-
     c[1, 2:Nyc] = 1.0/3.0
-
     c[Nxc, 2:Nyc] = 1.0/3.0
-
     c[2:Nxc, Nyc] = 1.0/3.0
 
     return c
@@ -24,19 +16,12 @@ def pressure_bc(Nxc, Nyc, c):
 def velocity_bc(i, j, u, v, Us, Un, Vw, Ve, Nyc, Nxc):
 
     u[i, 0] = 2.0 * Us - u[i, 1]
-
     u[i, Nyc+1] = 2.0 * Un - u[i, Nyc]
-
     u[0, j+1] = 0.0
-
     u[Nxc, j+1] = u[0, j+1]
-
     v[0, j] = 2.0 * Vw - v[1, j]
-
     v[Nxc+1, j] = 2.0 * Ve - v[Nxc, j]
-
     v[i+1, 0] = 0.0
-
     v[i+1, Nyc] = v[i+1, 0]
 
     return v, u
@@ -45,11 +30,8 @@ def velocity_bc(i, j, u, v, Us, Un, Vw, Ve, Nyc, Nxc):
 def pressure_ghosts(Nxc, Nyc, P):
 
     P[0:Nxc+2, 0] = 0
-
     P[0:Nxc+2, Nyc+1] = 0
-
     P[0, 0:Nyc+2] = 0
-
     P[Nxc+1, 0:Nyc+2] = 0
 
     return P
@@ -58,11 +40,8 @@ def pressure_ghosts(Nxc, Nyc, P):
 def vorticity_bc(i, j, Nxc, Nyc, dx, dy, phi, vorticity):
 
     vorticity[i, 0] = - 2.0 * phi[i, 1]/(dx*dy)
-
     vorticity[i, Nyc + 1] = -2.0 * phi[i, Nyc]/(dx * dy) - 2.0/dx
-
     vorticity[0, j] = -2.0 * phi[1, j]/(dx*dy)
-
     vorticity[Nxc+1, j] = - 2.0 * phi[Nxc, j]/(dx*dy)
 
     return vorticity
